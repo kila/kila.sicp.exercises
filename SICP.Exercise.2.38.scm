53ebdd94e8368934d338d8a1de327140367804a4
Kip Landergren <kila.consulting@gmail.com>
2010-03-10 12:12:09 -0500

    committing test for wed

---
+++ SICP.Exercise.2.38.scm
@@ -0,0 +1,13 @@
+;;; Exercise 2.38
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))
+
+(accumulate / 1 (list 1 2 3))
